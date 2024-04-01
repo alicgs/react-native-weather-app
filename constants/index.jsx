9e70e8470156fd0657170bb3c51a3565849fb82c
Ali Çığış <139799583+alicgs@users.noreply.github.com>
2024-04-02 01:14:59 +0300

    fnsh

---
+++ constants/index.jsx
@@ -0,0 +1,20 @@
+export const apiKey= '876c17351ac5457395c142824240104'
+
+export const weatherImages = {
+    'Partly cloudy': require("../assets/images/cloudy-day (1).png"),
+    'Moderate rain': require("../assets/images/Moon cloud mid rain.png"),
+    'Patchy rain possible': require("../assets/images/Sun cloud angled rain.png"),
+    'Sunny': require("../assets/images/sun (2).png"),
+    'Clear': require("../assets/images/sun (2).png"),
+    'Overcast': require("../assets/images/cloudy.png"),
+    'Cloudy': require("../assets/images/cloudy.png"),
+    'Light rain': require("../assets/images/Moon cloud mid rain.png"),
+    'Moderate rain at times': require("../assets/images/Moon cloud mid rain.png"),
+    'Heavy rain': require("../assets/images/storm.png"),
+    'Heavy rain at times': require("../assets/images/storm.png"),
+    'Moderate or heavy freezing rain': require("../assets/images/storm.png"),
+    'Moderate or heavy rain shower': require("../assets/images/storm.png"),
+    'partly cloudy': require("../assets/images/cloudy-day (1).png"),
+    'partly cloudy': require("../assets/images/cloudy-day (1).png"),
+    'other': require("../assets/images/Sun cloud angled rain.png")
+}
---
+++ src/navigation/appNavigation.jsx
@@ -0,0 +1,22 @@
+import {  LogBox, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeScreen from '../screens/HomeScreen'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
+
+LogBox.ignoreLogs([
+    'Non-serializable values were found in the navigation state',
+]);
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
